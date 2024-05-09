b730eb4e1ddc4c3dd2a512f9fd97a12d72bbf6ae
Spyeedy <spyeedy8@gmail.com>
2024-05-09 14:34:21 +0800

    Smeltinery displays player's inventory now

--- common/src/main/java/spyeedy/mods/spytwenohone/client/inventory/SmeltineryInvScreen.java
+++ common/src/main/java/spyeedy/mods/spytwenohone/client/inventory/SmeltineryInvScreen.java
@@ -3,21 +3,26 @@ package spyeedy.mods.spytwenohone.client.inventory;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import spyeedy.mods.spytwenohone.SpyTwentyOhOne;
 import spyeedy.mods.spytwenohone.container.SmeltineryContainer;
 
 public class SmeltineryInvScreen extends AbstractContainerScreen<SmeltineryContainer> {
+
+	static final ResourceLocation GUI_TEX = new ResourceLocation(SpyTwentyOhOne.MOD_ID, "textures/gui/smeltinery.png");
 
 	public SmeltineryInvScreen(SmeltineryContainer menu, Inventory playerInventory, Component title) {
 		super(menu, playerInventory, title);
 	}
 
 	@Override
-	protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
-	}
+	protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {}
 
 	@Override
-	public boolean isPauseScreen() {
-		return super.isPauseScreen();
+	protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+		int i = (this.width - this.imageWidth) / 2;
+		int j = (this.height - this.imageHeight) / 2;
+		guiGraphics.blit(GUI_TEX, i, j, 0, 0, this.imageWidth, this.imageHeight);
 	}
 }
--- common/src/main/java/spyeedy/mods/spytwenohone/container/SmeltineryContainer.java
+++ common/src/main/java/spyeedy/mods/spytwenohone/container/SmeltineryContainer.java
@@ -3,6 +3,7 @@ package spyeedy.mods.spytwenohone.container;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
 public class SmeltineryContainer extends AbstractContainerMenu {
@@ -12,6 +13,16 @@ public class SmeltineryContainer extends AbstractContainerMenu {
 	public SmeltineryContainer(int containerId, Inventory playerInv) {
 		super(SpyTooContainers.SMELTINERY.get(), containerId);
 		this.playerInv = playerInv;
+
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 9; x++) {
+				this.addSlot(new Slot(playerInv, x + y * 9 + 9, 8 + x * 18, 84 + y * 18));
+			}
+		}
+
+		for(int x = 0; x < 9; ++x) {
+			this.addSlot(new Slot(playerInv, x, 8 + x * 18, 142));
+		}
 	}
 
 	@Override
